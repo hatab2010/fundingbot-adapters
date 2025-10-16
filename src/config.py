17eaf24cdc5d95772031ca9785b535a133dc94c1
Leontiy <hatab200@gmail.com>
2025-10-17 00:52:03 +0300

    feat: добавлен клиент Bitget и конфигурация окружения
    Созданы файлы конфигурации для Bitget, включая переменные окружения для API ключей. Реализован клиент Bitget, который обрабатывает ставки финансирования для USDT-свопов, с валидацией данных через Pydantic.

---
+++ src/config.py
@@ -0,0 +1,7 @@
+import os
+
+TESTNET = os.getenv("TESTNET") == "True"
+
+BITGET_API_KEY = os.getenv("BITGET_API_KEY")
+BITGET_SECRET = os.getenv("BITGET_SECRET")
+BITGET_PASSWORD = os.getenv("BITGET_PASSWORD")
